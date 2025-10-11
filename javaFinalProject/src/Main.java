0b78a056e6ec202f6d8c1602408f9212968822f6
Sofia <sonechka.podobriy@gmail.com>
2025-10-12 01:23:04 +0300

    Собрано меню, команды оаботают

--- javaFinalProject/src/Main.java
+++ javaFinalProject/src/Main.java
@@ -1,57 +1,56 @@
-import CustomClass.Car;
+import customClass.Car;
+import readingStrategy.*;
+import search.BinarySearch;
+import sorting.ThreadPoolSort;
 import userInterface.UserInterface;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
 
-class Main{
-    public static void main(String[] args) {
-        //TODO пока разбиение на задачи для группы видится как:
-        // София 1) main функция: тут реализовать интерфейс для пользователя, убрать все в методы, тестить все остальные ветки
-        // + комменты
-        // + валидация
-        // Алексей 2) CustomClass + builder (папка customClass)
-        // Павел 3) Методы чтения входных данных + strategy pattern (папка readingInfo)
-        // Виталий 4) Бинарный поиск (папка binarySearch)
-        // Андрей 5) Сортировка  (папка sorting)
-        // Так же каждый делает по файлу для теста своих методов в папке tests.
-        Scanner inputScanner = new Scanner(System.in);
+class Main {
+    private final Scanner inputScanner;
+    private final UserInterface ui;
+    private List<Car> cars;
 
-        UserInterface ui = new UserInterface();
+    private ReadingStrategy inputStrategy;
+    private Comparator<Car> naturalComparator;
+
+
+    // Конструктор
+    public Main() {
+        this.inputScanner = new Scanner(System.in);
+        this.ui = new UserInterface();
+        this.cars = new ArrayList<>();
+        this.naturalComparator = Car::compareTo;
+    }
+
+    public void run() {
 
         boolean runProgram = true;
 
-        while (runProgram){
+        while (runProgram) {
             ui.printCommands();//вывод пользовательского меню
             String commandNumber = inputScanner.nextLine();//получаем номер команды от пользователя
-            //Car[] cars = ;
 
-
-            switch (commandNumber){
-                //TODO все "кейсы" можно заменить на константы или enum, в целом как будто можно и так оставить
-                //TODO функции чтения как будто по логике должны только возвращать массив Object, создание массива CustomClass
-                // можно реализовать отдельно в том же CustomClass и передавать в метод массив Object. Какая-то валидация там уже не нужна
-                // так как мы должны будем послать уже на этом этапе
-                //все файлы для чтения входных данных пока лежат в папке readingInfo
+            switch (commandNumber) {
                 case "1"://Ввод данных
-                    //TODO добавить метод в UserInput, которая возвращает массив Object для дальнейшей обработки (Object потому юзер будет вводить String, double, int)
-                    //TODO Так же имеет смысл видимо в той же функции прописать логику с длиной массива (у меня в голове длина массива от юзера==количеству инпутов
-                    // + мне кажется надо проверять чтобы по типам вышло все ровно, то есть мы ждем от юзера равное количество разных типов String/double/int
-                    // и если не получаем от него, как пример, ровно три int три String и три double, то говорим что мы таким не занимаемся и идем на следующий виток)
-                    // То есть в итоге метод должен уметь:
-                    // 1) принимать длину массива
-                    // 2) делать необходимое количество запросов на данные у юзера
-                    // 3) валидировать что юзер не ввел гадостей/неверное количество ожидаемых данных
                     ui.chooseInputType();
                     String inputType = inputScanner.nextLine();
-                    switch(inputType){
+
+                    switch (inputType) {
                         case "1"://Данные из консоли
-                            //вызов функции чтения данных из консоли
+                            inputStrategy = new UserInputStrategy();
+                            cars = inputStrategy.getData();
                             break;
                         case "2"://Данные из файла
-                            //вызов функции чтения из файла
+                            inputStrategy = new ReadingFromFileStrategy();
+                            cars = inputStrategy.getData();
                             break;
                         case "3"://Случайные данные
-                            //вызов функции случ. данных
+                            inputStrategy = new RandomDataStrategy();
+                            cars = inputStrategy.getData();
                             break;
                         case "4"://Назад в меню
                             break;
@@ -59,36 +58,76 @@ class Main{
                             System.out.println("Введенная команда не поддерживается.");
                     }
 
-                    break;
-                case "2"://Получить отсортированные данные
+                    if (cars != null && !cars.isEmpty()) {
+                        ThreadPoolSort sort = new ThreadPoolSort<>();
+                        sort.parallelSort(cars.toArray(new Car[0]));
+                        System.out.println("Данные успешно отсортированы.");
+                    } else {
+                        System.out.println("Нет данных для сортировки.");
+                    }
 
+                    break;
+
+                case "2"://Получить отсортированные данные
+                    if (cars == null || cars.isEmpty()) {
+                        System.out.println("Нет данных для отображения.");
+                    } else {
+                        System.out.println("Отсортированные данные:");
+                        for (int i = 0; i < cars.size(); i++) {
+                            System.out.println((i + 1) + ". " + cars.get(i));
+                        }
+                    }
                     break;
 
                 case "3"://Поиск элемента
+                    if (cars == null || cars.isEmpty()) {
+                        System.out.println("Нет данных для поиска. Сначала введите данные.");
+                        break;
+                    }
+                    //TODO пользователь вводит для поиска данные
+                    Car key = new Car.Builder().setModel("BMW").setMaxSpeed(250.0).setWeight(1600).build();
+                    //TODO если решим делать поиск по разным полям отдельно
+                    //Comparator<Car> byModel = Comparator.comparing(Car::getModel);
 
+                    int index = BinarySearch.search(cars, key, naturalComparator);
+                    if(index != -1)
+                        System.out.println("Номер элемента в массиве: " + (index+1));
+                    else
+                        System.out.println("Элемент не найден.");
 
                     break;
+
                 case "4":
                     runProgram = false;
                     break;
+
                 default:
                     System.out.println("Введенная команда не поддерживается.");
-
             }
 
-
-            //TODO тут прокидывать массив Object в наш CustomClass метод для преобразования в массив типа CustomClass[]
-
-            //TODO после получения массива CustomClass[] отправлять его в функцию многопоточной сортировки (пока в процессе,
-            // думаю что будет лежать в папке sorting). Так же можно решить делать сортировку всегда
-            // или давать пользователю промт (типа "Посортируем?")
         }
     }
 
-
-
+    public static void main(String[] args) {
+        new Main().run();
+    }
 
 }
 
+//TODO пока разбиение на задачи для группы видится как:
+// София 1) main функция: тут реализовать интерфейс для пользователя, убрать все в методы, тестить все остальные ветки
+// + комменты
+// + валидация
+// Алексей 2) CustomClass + builder (папка customClass)
+// Павел 3) Методы чтения входных данных + strategy pattern (папка readingInfo)
+// Виталий 4) Бинарный поиск (папка binarySearch)
+// Андрей 5) Сортировка  (папка sorting)
+// Так же каждый делает по файлу для теста своих методов в папке tests.
+
+//Вопросы
+//1. Просмотр отсортированных данных по отдельной команде
+//2. При сортировке вывод на английском языке
+//3. Поиск в каком виде должен быть? по модели и тд, пользователь вводит модель для поиска или как, сам ли он выбирает вообще
+//4. В свиче я выбираю по цифрам, могу переделать по enum, если это имеет смысл
 
 
