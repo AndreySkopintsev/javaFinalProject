24a17312a9c06f47be096b291a86d1b92cd1ffe1
Pavel Vasilyev <pavel.vas@mail.ru>
2025-10-12 13:02:11 +0300

    Add validation to file input

--- javaFinalProject/src/readingStrategy/ReadingFromFileStrategy.java
+++ javaFinalProject/src/readingStrategy/ReadingFromFileStrategy.java
@@ -8,31 +8,49 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
 
 public class ReadingFromFileStrategy implements ReadingStrategy{
     @Override
     public List<Car> getData() {
-        List<String> contentFromFile = readFile();
-        int size = contentFromFile.size();
+        boolean isDataOk = false;
+        List collection = null;
 
-        List<Car> collection = new ArrayList<>(size);
-        for (String s : contentFromFile) {
-            Map<String, Object> fields;
-            try {
-                fields = parseString(s);
-            } catch (Exception e) {
-                throw new RuntimeException(e);
+        while (!isDataOk) {
+            List<String> contentFromFile = readFile();
+            int size = contentFromFile.size();
+            collection = new ArrayList<>(size);
+
+            for (String s : contentFromFile) {
+                Map<String, Object> fields;
+                String model = null;
+                double maxSpeed = 0;
+                int weight = 0;
+                try {
+                    fields = parseString(s);
+                    model = fields.get("model").toString();
+                    maxSpeed = (double) fields.get("maxSpeed");
+                    weight = (int) fields.get("weight");
+                    isDataOk = true;
+                } catch (Exception e) {
+                    isDataOk = false;
+                }
+
+                try {
+                    Car car = new Car.Builder()
+                            .setModel(model)
+                            .setMaxSpeed(maxSpeed)
+                            .setWeight(weight)
+                            .build();
+                    collection.add(car);
+                } catch (RuntimeException e) {
+                    System.out.println("Ошибка при чтении файла");
+                }
             }
-            String model = fields.get("model").toString();
-            double maxSpeed = (double) fields.get("maxSpeed");
-            int weight = (int) fields.get("weight");
-            Car car = new Car.Builder()
-                    .setModel(model)
-                    .setMaxSpeed(maxSpeed)
-                    .setWeight(weight)
-                    .build();
-            collection.add(car);
         }
 
         return collection;
